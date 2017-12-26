f99b82736970700ea70620f09c6602806fa8f372
Sergey Lozhechnikov <s.lozhechnikov@astoundcommerce.com>
2017-12-26 11:29:18 +0200

    lab1 task

---
+++ Lab1/Basket.js
@@ -0,0 +1,34 @@
+var prodcuts = [{
+	name : "test",
+	price : 12.9,
+	inventory : 20
+}, {
+	name : "test2",
+	price : 30,
+	inventory : 80
+}];
+
+class ProductLineItem {
+	constructor(product) {
+		//implement
+	}
+}
+
+
+var basket = (function(){
+	//implement
+	return {
+		addProduct : function(productID){
+			//implement
+		},
+		removeProduct : function(productID){
+			//implement
+		},
+		updateProductQuantity : function(productID, quantity) {
+			//implement
+		},
+		getTotalPrice : function(){
+			//implement
+		}
+	}
+})();
